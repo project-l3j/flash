b8849dbc29c57050fdd7536e809f1b6bc45a7963
Luke Chandler <lhchandler4@gmail.com>
2019-04-16 13:52:31 -0700

    js logic for homepage

--- js/display-decks.js
+++ js/display-decks.js
@@ -4,11 +4,41 @@ saveDecks();
 
 //Check the number of decks in local storage
 console.log('localStorage.length', localStorage.length);
-var checkLengthOfDecksInLocalStorage = JSON.parse(localStorage.getItem('decks'));
-console.log(checkLengthOfDecksInLocalStorage.length);
+var decksInLocalStorage = JSON.parse(localStorage.getItem('decks'));
+console.log(decksInLocalStorage.length);
 
 
-//Based on the number of decks in local storage, create the number of rows needed
+//Based on the number of decks in local storage, create the number of rows needed. decksInLocalStorage % 2
+//If the remainder is 0 then number of decks is even
+//If the remainder is not 0 then number of decks is odd
+
+var isEven = null;
+if (decksInLocalStorage.length % 2 === 0) {
+  isEven = true;
+} else {
+  isEven = false;
+}
+
+if (isEven) {
+  var numOfRows = decksInLocalStorage.length / 2;
+}
+
+// length of loop: numOfRows
+
+// after loop, add one more row w/ create new deck
+
+for (let i = 0; i < decksInLocalStorage.length; i++) {
+  // if even number of decks, create a row
+  if (decksInLocalStorage.length % 2 === 0) {
+    // create row
+    // then append column w/ deck[i]
+    // then append column w/ deck[i + 1]
+  } 
+
+  
+  // then create second row
+  // append column w/ 'create new deck' card
+}
 
 
 //Create a column
